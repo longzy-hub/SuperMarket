fe0b2309eb46e01b0806d0154643ea6647b1244e
longzy-hub <2587234011@qq.com>
2021-02-02 18:56:46 +0800

    访问jsp

--- SuperMarket-api/src/main/java/com/lzy/controller/TestController.java
+++ SuperMarket-api/src/main/java/com/lzy/controller/TestController.java
@@ -4,17 +4,26 @@ import com.lzy.controller.viewobject.TestVo;
 import com.lzy.response.CommonReturnType;
 import com.lzy.validator.ValidatorApi;
 import org.apache.ibatis.io.ResolverUtil;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 
 //controller + ResponseBody
-@RestController
+//@RestController
+@Controller
 public class TestController {
 
     @Resource
     private ValidatorApi validator;
+
+    // 测试页面
+    @RequestMapping("/index.page")
+    public String indexPage(){
+        return "index";
+    }
 
     // 测试校验器
     @GetMapping("/validateTest")
---
+++ SuperMarket-web/src/main/java/com/lzy/Application.java
@@ -0,0 +1,18 @@
+package com.lzy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import tk.mybatis.spring.annotation.MapperScan;
+
+// 启动类
+@SpringBootApplication // 扫描所有包
+@MapperScan(basePackages = "com.lzy.mapper")
+@ComponentScan(basePackages = {"com.lzy"})
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+
+}
